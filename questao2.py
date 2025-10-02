1f54c6a97e08cb16b2e88fd6e7c2a8d967913163
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2025-10-02 20:31:58 -0300

    Update questao2.py

--- questao2.py
+++ questao2.py
@@ -1,20 +1,20 @@
 import threading #importa o módulo de threads pra rodar as tarefas simultaneamente
-import time #função de tempo 
-import random #usado nesse código para gerar os tempos aleatoriamente
+import time 
+import random
 
 compilador = threading.Semaphore(1)  #semáforo que permite apenas 1 thread por vez (só um exclusivo compilando) 
 banco_dados = threading.Semaphore(2) #semáforo que permite até 2 threads (2 podem acessar o BD simultaneamente)
 print_lock = threading.Lock() #evitar que threads imprimam ao mesmo tempo
 
-class Programador(threading.Thread): #define a classe Programador herdando de threading.Thread
-    def __init__(self, id): #método construtor, self, id = parametros do construtor
-        super().__init__() #chama o construtor __init__() da classse mãe threading.Thread
+class Programador(threading.Thread):
+    def __init__(self, id): 
+        super().__init__()
         self.id = id
     
     def pensando(self): #método do momento que o programador está pensando
         with print_lock:
             print(f"Programador {self.id} está pensando...")
-        time.sleep(random.uniform(0, 2)) #dorme por um tempo aleatorio, tempo que pensa
+        time.sleep(random.uniform(0, 2))
     
     def compilando(self): #método do momento que o programador está compilando
         with print_lock:
@@ -29,17 +29,17 @@ class Programador(threading.Thread): #define a classe Programador herdando de th
             self.pensando()
             
             with print_lock:
-                print(f"Programador {self.id} quer trabalhar...")
+                print(f"Programador {self.id} quer trabalhar!")
                 time.sleep(random.uniform(0, 2))
             
             banco_dados.acquire() #tenta pegar permissão de acesso ao BD, se já tem 2 programadores, thread bloqueia
             with print_lock:
-                print(f"Programador {self.id} acessou o banco de dados.")
+                print(f"Programador {self.id} está no banco de dados agora.")
                 time.sleep(random.uniform(1, 3))
             
             compilador.acquire() #tenta pegar o compilador e se já tiver um programador compilando, bloqueia
             with print_lock:
-                print(f"Programador {self.id} pegou o compilador.")
+                print(f"Programador {self.id} está no compilador agora.")
                 time.sleep(random.uniform(1, 3))
             
             self.compilando()
@@ -50,7 +50,7 @@ class Programador(threading.Thread): #define a classe Programador herdando de th
             
             banco_dados.release() #vaga liberada ao BD
             with print_lock:
-                print(f"Programador {self.id} saiu do banco de dados.")
+                print(f"Programador {self.id} liberou o banco de dados.")
 
 #criar e iniciar os programadores
 programadores = [Programador(i) for i in range(1, 6)]
